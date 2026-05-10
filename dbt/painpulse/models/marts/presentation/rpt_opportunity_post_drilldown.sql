abf5a9122d43099a30d8eb56b36e455c7bfedbf0
Anu <anuoluwa.apiti@gmail.com>
2026-05-10 01:48:50 -0400

    Superset presentation layer

---
+++ dbt/painpulse/models/marts/presentation/rpt_opportunity_post_drilldown.sql
@@ -0,0 +1,39 @@
+with ranked as (
+
+    select
+        mp.opportunity_id,
+        co.canonical_title,
+        mp.post_id,
+        fp.title,
+        fp.body,
+        fp.subreddit,
+        fp.created_at_utc,
+        fp.score,
+        fp.num_comments,
+        concat('https://reddit.com/comments/', fp.post_id) as reddit_url,
+        row_number() over (
+            partition by mp.opportunity_id
+            order by fp.score desc, fp.num_comments desc, fp.created_at_utc desc
+        ) as post_rank
+    from {{ ref('matched_pain_posts') }} mp
+    join {{ ref('fct_pain_posts') }} fp
+      on mp.post_id = fp.post_id
+    left join {{ ref('canonical_opportunities') }} co
+      on mp.opportunity_id = co.opportunity_id
+
+)
+
+select
+    opportunity_id,
+    canonical_title,
+    post_id,
+    title,
+    body,
+    subreddit,
+    created_at_utc,
+    score,
+    num_comments,
+    reddit_url,
+    post_rank
+from ranked
+where post_rank <= 10
---
+++ dbt/painpulse/models/marts/presentation/rpt_opportunity_trends.sql
@@ -0,0 +1,26 @@
+with daily as (
+
+    select
+        mp.opportunity_id,
+        co.canonical_title,
+        date_trunc('day', fp.created_at_utc) as activity_date,
+        count(*) as post_count,
+        avg(fp.score) as avg_post_score,
+        avg(fp.num_comments) as avg_num_comments
+    from {{ ref('matched_pain_posts') }} mp
+    join {{ ref('fct_pain_posts') }} fp
+      on mp.post_id = fp.post_id
+    left join {{ ref('canonical_opportunities') }} co
+      on mp.opportunity_id = co.opportunity_id
+    group by 1, 2, 3
+
+)
+
+select
+    opportunity_id,
+    canonical_title,
+    activity_date,
+    post_count,
+    avg_post_score,
+    avg_num_comments
+from daily
